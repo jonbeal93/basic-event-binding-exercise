a1df190b90cb530cd8d671e6fb915820c9a79967
jonbeal93 <jonbeal93@gmail.com>
2022-09-26 19:36:34 -0500

    Make an event binding that will console log upon clicking button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basic-event-binding-exercise';
+  onShow(){
+    console.log("this works")
+  }
 }
